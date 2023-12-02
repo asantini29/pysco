c39940187b449e26787ebf0c49c4b0b1e44684cb
Alessandro Santini <alesandro.santini@aei.mpg.de>
2023-12-02 01:56:50 +0100

    code

---
+++ pysco/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+import logging
+
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+from .pysco import __name__
+from .pysco import __version__
+from .pysco import __author__
+from .pysco import __author_email__
+
+from .pysco import *
