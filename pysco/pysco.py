7ed201d70fb6c32ffdc8be82fceca49f5d261105
Alessandro Santini <alesandro.santini@aei.mpg.de>
2024-03-20 10:57:41 +0100

    remove default

--- pysco/pysco.py
+++ pysco/pysco.py
@@ -7,9 +7,3 @@ if __name__ != "__main__":
     __author_email__        = "alessandro.santini@aei.mpg.de"
     __description__         = "PYthon ShortCuts & Others: plotting routines I like to use & overall collection of useful snippets"
     __license__             = "MIT"
-
-
-from .plot import default_plotting, custom_color_cycle
-
-default_plotting()
-custom_color_cycle()
