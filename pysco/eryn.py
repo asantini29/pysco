8a47b832c1ebf4db3191398acf61c19cc5f8521d
Alessandro Santini <asantini@saraswati.aei.mpg.de>
2024-06-04 16:33:46 +0200

    diagnostic class

--- pysco/eryn.py
+++ pysco/eryn.py
@@ -54,6 +54,377 @@ def adjust_covariance(samp, ndim, svd=False, idxs=1):
                 move.all_proposal[key].svd = svd
             else:
                 move.all_proposal[key].scale = cov
+
+
+class DiagnosticPlotter:
+    """
+    A class for generating diagnostic plots based on the state of the sampler.
+
+    Attributes:
+    - sampler: The sampler object to use for the diagnostic plots.
+    - path: The path to save the diagnostic plots.
+    - truths: A dictionary containing the true values for each branch.
+    - labels: A dictionary containing the labels for each parameter in each branch.
+    - transform_all: A ``transform_container`` object containing the transformation functions for each branch.
+    - true_logl: The true log likelihood value.
+    - discard: The fraction of iterations to discard before plotting.
+    - suffix: A suffix to append to the filenames of the diagnostic plots.
+
+    Methods:
+    - setup(sampler): Set up the diagnostic plotter with the given sampler.
+    - __call__(**kwargs): Perform various plotting operations based on the current state of the sampler.
+    - plot_corners(samples, logl, trace=True, **kwargs): Plot corner plots and trace plots for the given samples.
+    - plot_acceptance(): Plot the acceptance fraction for each update step in the MCMC sampling process.
+    - plot_leaves_hist(): Plot the histogram of the number of leaves for each temperature.
+    """
+
+    def __init__(self, sampler, path, truths, labels, transform_all=None, true_logl=None, discard=0.3, suffix='') -> None:
+        self.sampler = sampler
+        self.path = path
+        self.truths = truths
+        self.labels = labels
+        self.transform_all = transform_all
+        self.discard = discard
+        self.true_logl = true_logl
+        self.suffix = suffix
+        self.setup(sampler)
+
+    def setup(self, sampler):
+        """
+        Set up the diagnostic plotter with the given sampler.
+
+        Parameters:
+        - sampler: The sampler object to use for the diagnostic plots.
+
+        Returns:
+        None
+        """
+        if self.sampler is None and sampler is not None:
+            # Retrieve information from the sampler
+            print('Retrieving information from the sampler.')
+            self.sampler = sampler
+            self.nw = sampler.nwalkers
+            self.nt = sampler.ntemps
+            self.nleaves_max = sampler.nleaves_max
+
+            if hasattr(sampler, 'nleaves_min'):
+                self.nleaves_min = sampler.nleaves_min
+            else:
+                self.nleaves_min = self.nleaves_max
+
+            self.rj_branches = [key for key in sampler.branch_names if self.nleaves_min[key] != self.nleaves_max[key]]
+
+            self.has_rj = len(self.rj_branches) > 0
+
+            self.tempcolors = pysco.plot.get_colors_from_cmap(self.nt, cmap='inferno', reverse=False)
+
+            if not isinstance(self.truths, dict):
+                self.truths = np.atleast_2d(self.truths)
+                self.truths = {key: self.truths[i] for i, key in enumerate(sampler.branch_names)}
+            
+            if not isinstance(self.labels, dict):
+                if not isinstance(self.labels[0], list):
+                    self.labels = [self.labels]
+                self.labels = {key: self.labels[i] for i, key in enumerate(sampler.branch_names)}
+
+            # Acceptance fraction diagnostics
+            self.steps = []
+            self.acceptance_all = []
+            self.acceptance_moves = {}
+
+            for i, move in enumerate(sampler.moves):
+                try:
+                    label = move.label
+                except:
+                    label = f'Move {i}'
+                self.acceptance_moves[label] = []
+
+            if self.has_rj:
+                self.rj_acceptance_all = []
+                self.rj_acceptance_moves = {}
+
+                for i, move in enumerate(sampler.rj_moves):
+                    try:
+                        label = move.label
+                    except:
+                        label = f'Move {i}'
+                    self.rj_acceptance_moves[label] = []
+
+        else:
+            # The sampler is already set up or a valid sampler was not provided
+            pass
+
+    def __call__(self, return_samples=False, **kwargs):
+        """
+        Perform various plotting operations based on the current state of the sampler.
+
+        Parameters:
+        - kwargs: Additional keyword arguments to be passed to the plotting functions.
+
+        Returns:
+        None
+        """
+        iteration = self.sampler.iteration
+        samples = self.sampler.get_chain(discard=int(self.discard * iteration), thin=1)
+        logl = self.sampler.get_log_like(discard=int(self.discard * iteration), thin=1)
+
+        # Plot the corner and trace plots
+        self.plot_corners(samples, logl[:, 0].flatten(), **kwargs)
+
+        # Plot the acceptance fraction evolution
+        self.plot_acceptance()
+
+        # Plot the RJ diagnostics
+        if self.has_rj:
+            self.plot_leaves_hist()
+
+        # Plot the integrated autocorrelation time evolution
+        self.plot_act_evolution(N=10, all_T=False, **kwargs)
+
+        # Plot the log likelihood evolution
+        self.plot_logl_evolution(logl)
+
+        if return_samples:
+            return samples, logl
+
+    def plot_corners(self, samples, logl, trace=True, **kwargs):
+        """
+        Plot corner plots and trace plots for the given samples.
+
+        Args:
+            samples (dict): A dictionary containing the samples for each branch.
+            logl (array-like): The log likelihood values.
+            trace (bool, optional): Whether to plot trace plots. Defaults to True.
+            **kwargs: Additional keyword arguments to be passed to the corner plot function.
+
+        Returns:
+        None
+        """
+        for key in self.sampler.branch_names:
+            ndims = self.sampler.ndims[key]
+
+            if self.transform_all is not None:
+                    samples_here = self.transform_all[key].both_transforms(samples[key])
+            else:
+                samples_here = samples[key]
+
+            chain = get_clean_chain(samples_here, ndim=ndims)
+
+            truths_here = self.truths[key]
+            labels_here = self.labels[key]
+
+            if self.nleaves_min[key] == self.nleaves_max[key]:
+                truths_here = np.append(truths_here, self.true_logl)
+                labels_here = np.append(labels_here, r'$\log{\mathcal{L}}$')
+
+                chain = np.column_stack((chain, logl))
+
+                fig = pysco.plot.corner(chain,
+                                        truths=truths_here,
+                                        labels=labels_here,
+                                        save=True,
+                                        custom_whspace=0.15,
+                                        filename=self.path + key + '_cornerplot' + self.suffix,
+                                        dpi=150,
+                                        linestyle='-',
+                                        **kwargs
+                                        )
+            plt.close()
+
+            if trace:
+                fig, axs = plt.subplots(self.sampler.ndims[key], 1)
+                fig.set_size_inches(20, 20)
+
+                for i in range(ndims):
+                    for walk in range(self.nw):
+                        axs[i].plot(samples_here[:, 0, walk, :, i], ls='-', alpha=0.9, lw=1)
+
+                    axs[i].set_ylabel(self.labels[key][i])
+
+                    if truths_here[i] is not None:
+                        axs[i].axhline(truths_here[i], color='k', ls='--', lw=2)
+
+                fig.savefig(self.path + key + '_traceplot' + self.suffix, dpi=150)
+                plt.close()
+
+    def plot_acceptance(self):
+        """
+        Plot the acceptance fraction for each update step in the MCMC sampling process.
+        If the `has_rj` attribute is True, it also plots the RJ acceptance fraction.
+
+        Returns:
+        None
+        """
+        fig = plt.figure()
+        self.steps.append(self.sampler.iteration)
+        steps = np.array(self.steps)
+        self.acceptance_all.append(np.mean(self.sampler.acceptance_fraction[0]))
+
+        plt.plot(self.steps, self.acceptance_all, color='k', label='Total', marker='x', lw=1)
+
+        for i, move in enumerate(self.sampler.moves):
+            try:
+                label = move.label
+            except:
+                label = f'Move {i}'
+            self.acceptance_moves[label].append(np.mean(move.acceptance_fraction[0]))
+
+            acceptance = np.array(self.acceptance_moves[label])
+
+            nanmask = np.isnan(acceptance)
+
+            plt.plot(steps[~nanmask], acceptance[~nanmask], label=label, marker='x', lw=1)
+
+        plt.axhline(0.234, color='k', ls='--', lw=2, label=r'$\alpha=23.4\%$')
+
+        plt.xlabel(r'$N_{\rm steps}$')
+        plt.ylabel(r'Acceptance fraction')
+        plt.legend()
+        fig.savefig(self.path + 'acceptance' + self.suffix, dpi=150)
+        plt.close()
+
+        if self.has_rj:
+            fig = plt.figure()
+            self.rj_acceptance_all.append(np.mean(self.sampler.rj_acceptance_fraction[0]))
+
+            plt.plot(self.steps, self.rj_acceptance_all, color='k', label='Total', marker='x')
+
+            for i, move in enumerate(self.sampler.rj_moves):
+                try:
+                    label = move.label
+                except:
+                    label = f'Move {i}'
+                self.rj_acceptance_moves[label].append(np.mean(move.acceptance_fraction[0]))
+
+                nanmask = np.isnan(self.rj_acceptance_moves[label])
+
+                plt.plot(self.steps[~nanmask], self.rj_acceptance_moves[label][~nanmask], label=label, marker='x')
+
+            plt.xlabel(r'$N_{\rm steps}$')
+            plt.ylabel(r'RJ Acceptance fraction')
+            plt.legend()
+            fig.savefig(self.path + 'acceptance_rj' + self.suffix, dpi=150)
+            plt.close()
+
+    def plot_leaves_hist(self):
+        """
+        Plot the histogram of the number of leaves for each temperature.
+
+        This method plots a histogram of the number of leaves for each temperature in the `rj_branches` dictionary.
+        It uses the `sampler` object to get the number of leaves for each temperature.
+        The histogram is plotted using the `plt.hist` function from the `matplotlib.pyplot` module.
+        The plot includes temperature-specific colors and a legend for the colors.
+
+        Returns:
+        None
+        """
+        for key in self.rj_branches:
+            nleaves_rj = self.sampler.get_nleaves()[key]
+            bns = (np.arange(self.nleaves_min[key], self.nleaves_max[key] + 2) - 0.5)
+
+            fig = plt.figure()
+
+            for temp, tempcolor in enumerate(self.tempcolors):
+                plt.hist(nleaves_rj[:, temp].flatten(), bins=bns, histtype="stepfilled", edgecolor=tempcolor,
+                         facecolor=to_rgba(tempcolor, 0.2), density=True, ls='-', zorder=100 - temp)
+
+            plt.xlabel('Number of leaves')
+            plt.ylabel('Density')
+            # Create legend entry for temperature colors
+            legend_colors = [to_rgba(tempcolor, 0.2) for tempcolor in self.tempcolors[::-1]]
+            legend_labels = ['' for _ in range(len(self.tempcolors))]
+            legend_labels[0] = r' $T_i$'
+            legend_handles = [plt.Rectangle((0, 0), 1, 1, color=color) for color in legend_colors]
+            legend_entry = plt.legend(legend_handles, legend_labels, loc='upper right', ncol=len(self.tempcolors), mode='expand', frameon=False, framealpha=0, bbox_to_anchor=(0.8, 0.95))
+
+            # Add legend entry to the plot
+            plt.gca().add_artist(legend_entry)
+
+            fig.text(0.07, 0.083, f"Step: {self.sampler.iteration}", ha='left', va='top', fontfamily='serif', c='k')
+            #plt.title(key)
+            fig.savefig(self.path + 'leaves_' + key + self.suffix, dpi=150)
+            plt.close()
+    
+    def plot_act_evolution(self, N=10, all_T=False, **kwargs):
+        """
+        Plot the auto-correlation time evolution.
+
+        Parameters:
+        - N (int): Number of points to plot.
+        - all_T (bool): Whether to plot for all temperatures or just one.
+        - **kwargs: Additional keyword arguments to pass to `get_integrated_act` function.
+
+        Returns:
+        - None
+
+        Raises:
+        - None
+        """
+
+        fig = plt.figure()
+        toplim = 0
+
+        samples = self.sampler.get_chain(discard=0, thin=1)
+        Npoints = np.exp(np.linspace(np.log(min(100, self.sampler.iteration)), np.log(self.sampler.iteration), N)).astype(int)
+
+        for key, color in zip(samples.keys(), pysco.plot.get_colorslist(colors='colors10')):
+
+            chain = samples[key]
+            nsteps, nt, nw, nleaves, ndim = chain.shape
+
+            chain = chain.reshape(nsteps, nt, nw, nleaves * ndim)
+
+            ntemps = chain.shape[1] if all_T else 1
+            tau = np.empty(shape=(len(Npoints), ntemps, chain.shape[-1]))
+
+            try:
+                for i, N in enumerate(Npoints):
+                    tau[i] = get_integrated_act(chain[:N, :ntemps], average=True, **kwargs)
+
+                for temp in range(ntemps):
+                    where_max = np.argmax(tau[:, temp], axis=-1)
+
+                    to_plot =np.max(tau[:, temp], axis=-1)
+
+                    plt.loglog(Npoints, to_plot, color=color, marker='x', alpha=0.9, label=key + fr' - $T_{temp}$')
+
+                    toplim = max(toplim, 5 * max(tau[-1, temp, where_max]))
+                
+            except:
+                warnings.warn(f"Could not compute the auto-correlation times for the branch {key}.")
+
+        plt.loglog(Npoints, Npoints / 50, label=r'$\tau = N/50$', linestyle='--', color='black')
+
+        plt.xlabel(r'$N_{\rm steps}$')
+        plt.ylabel(r'$\tau$')
+
+        plt.ylim(0, toplim)
+        plt.legend()
+
+        fig.savefig(self.path + 'act_evolution' + self.suffix, dpi=150)
+
+    
+    def plot_logl_evolution(self, logl):
+        """
+        Plots the evolution of log-likelihood values.
+
+        Args:
+            logl (numpy.ndarray): Array of log-likelihood values.
+
+        Returns:
+            None
+        """
+        fig = plt.figure()
+        for walk in range(self.nw):
+            plt.plot(logl[:, 0, walk], ls='-', alpha=0.9, lw=1)
+
+        if self.true_logl is not None:
+            plt.axhline(self.true_logl, color='k', ls='--', lw=2)
+        plt.ylabel(r'$\log{\mathcal{L}}$')
+        fig.savefig(self.path + 'loglike_evolution' + self.suffix, dpi=150)
+
+        plt.close()
+
         
 def plot_diagnostics(samp, path, ndim, truths, labels, transform_all, acceptance_all=None, true_logl=None, trace_color=None, acceptance_moves=None, rj_acceptance_all=None, rj_acceptance_moves=None, rj_branches=[], nleaves_min=None, nleaves_max=None, moves_names=None, rj_moves_names=None, use_chainconsumer=False, suffix='', **kwargs):
     
